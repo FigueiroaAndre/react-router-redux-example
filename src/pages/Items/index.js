670fe8bf5cda913b13461fd7c8cd30327cbdc8d9
André Figueirôa <alfigueiroab@gmail.com>
2021-06-30 10:11:38 -0300

    items page now show the list of items

--- src/pages/Items/index.js
+++ src/pages/Items/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { ROUTES } from '../../routes';
 
 export default function Items() {
-    return (
-        <p>Items</p>
-    );
+    const items = useSelector(state => state.items);
+    const history = useHistory();
+
+    function redirectToDetailsPage(id) {
+        history.push(`${ROUTES.ITEM_DETAILS}/${id}`);
+    }
+
+    return items.itemList.map(item => (
+        <div key={item.id}>
+            <p><span style={{fontWeight: 'bold', display: 'inline'}}>{item.name}</span> cost: ${item.cost}</p>
+            <button onClick={(e) => redirectToDetailsPage(item.id)}>Details</button>
+            <button style={{ marginLeft: 20 }}>Add to Cart</button>
+            <hr/>
+        </div>
+    ));
 }
