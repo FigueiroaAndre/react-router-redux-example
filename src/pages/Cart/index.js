f5d564163f2f433a50f2d0e5c268c314aa2b64c3
André Figueirôa <alfigueiroab@gmail.com>
2021-06-30 18:51:37 -0300

    Cart pages show list of items added to cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Cart() {
+    const cart = useSelector(state => state.cart.cart);
+    const itemList = useSelector(state => state.items.itemList);
+    const mergedCartItemArray = cart.map(cartItem => {
+        const item = itemList.find(item => item.id === cartItem.id);
+        return {
+            id: cartItem.id,
+            amount: cartItem.amount,
+            name: item.name,
+            cost: item.cost
+        }
+    }); 
+
+    function totalCost() {
+        return mergedCartItemArray.reduce((accumulator, item) => {
+            return accumulator + item.amount * item.cost;
+        }, 0);
+    }
+
     return (
-        <p>Cart</p>
+        <React.Fragment>
+            <h1>Cart</h1>
+            <hr/>
+            {
+                mergedCartItemArray.map(item => {
+                    return (
+                        <div key={item.id}>
+                            <h2 style={{margin: '0', display: 'inline'}}>{item.name}</h2>
+                            <p style={{margin: '0', marginLeft: '20px', display: 'inline'}}>Amount: {item.amount}</p>
+                            <p style={{margin: '0', marginLeft: '20px', display: 'inline'}}>Unit cost: ${item.cost}</p>
+                            <p style={{margin: '0'}}>cost: <span style={{ fontWeight:'bold', fontSize:'2rem', color: 'darkgreen'}}>${item.cost * item.amount}</span></p>
+                            <hr/>
+                        </div>
+                    );
+                })
+            }
+            <h2>Total: <span style={{ fontWeight:'bold', fontSize:'2rem', color: 'darkgreen'}}>${totalCost()}</span></h2>
+        </React.Fragment>
     );
 }
