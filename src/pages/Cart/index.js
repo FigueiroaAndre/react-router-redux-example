9861883d2fc949a2fe60d6d40da4045eaeeee49c
André Figueirôa <alfigueiroab@gmail.com>
2021-06-30 19:46:17 -0300

    Add "clear cart" and "remove item from cart" functionalities

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { pushItem, decrementItem, clearCart, removeItem } from '../../store/reducers/cartReducer';
+import { ROUTES } from '../../routes';
 
 export default function Cart() {
     const cart = useSelector(state => state.cart.cart);
     const itemList = useSelector(state => state.items.itemList);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    function redirectToDetailsPage(id) {
+        history.push(`${ROUTES.ITEM_DETAILS}/${id}`);
+    }
+
     const mergedCartItemArray = cart.map(cartItem => {
         const item = itemList.find(item => item.id === cartItem.id);
         return {
@@ -22,7 +32,8 @@ export default function Cart() {
 
     return (
         <React.Fragment>
-            <h1>Cart</h1>
+            <h1 style={{marginBottom: '0'}}>Cart</h1>
+            <button onClick={() => dispatch(clearCart())}>CLEAR CART</button>
             <hr/>
             {
                 mergedCartItemArray.map(item => {
@@ -30,8 +41,12 @@ export default function Cart() {
                         <div key={item.id}>
                             <h2 style={{margin: '0', display: 'inline'}}>{item.name}</h2>
                             <p style={{margin: '0', marginLeft: '20px', display: 'inline'}}>Amount: {item.amount}</p>
+                            <button onClick={() => dispatch(pushItem(item.id))} style={{marginLeft: '10px'}}>+</button>
+                            <button onClick={() => dispatch(decrementItem(item.id))} style={{marginLeft: '5px'}}>-</button>
                             <p style={{margin: '0', marginLeft: '20px', display: 'inline'}}>Unit cost: ${item.cost}</p>
                             <p style={{margin: '0'}}>cost: <span style={{ fontWeight:'bold', fontSize:'2rem', color: 'darkgreen'}}>${item.cost * item.amount}</span></p>
+                            <button onClick={() => redirectToDetailsPage(item.id)}>Details</button>
+                            <button style={{ marginLeft: 20 }} onClick={() => dispatch(removeItem(item.id))}>Remove</button>
                             <hr/>
                         </div>
                     );
--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -3,8 +3,10 @@ const INITIAL_STATE = {
 }
 
 const cartActions = {
-    PUSH_ITEM: 'cart/add_item',
-    REMOVE_ITEM: 'cart/remove_item'
+    PUSH_ITEM: 'cart/add',
+    DECREMENT_ITEM: 'cart/decrement',
+    CLEAR_CART: 'cart/clear',
+    REMOVE_ITEM: 'cart/remove'
 }
 
 export function pushItem(id) {
@@ -14,6 +16,21 @@ export function pushItem(id) {
             id
         }
     };
+}
+
+export function decrementItem(id) {
+    return {
+        type: cartActions.DECREMENT_ITEM,
+        payload: {
+            id
+        }
+    }
+}
+
+export function clearCart() {
+    return {
+        type: cartActions.CLEAR_CART
+    }
 }
 
 export function removeItem(id) {
@@ -31,14 +48,29 @@ export default function cartReducer(state = INITIAL_STATE, action) {
             const isItemAlreadyInTheCart = !!state.cart.find(cartItem => cartItem.id === action.payload.id);
 
             if (isItemAlreadyInTheCart) {
-                const newCartArray =  state.cart.map(cartItem =>  cartItem.id === action.payload.id ? {...cartItem, amount: cartItem.amount + 1} : cartItem);
-                return { ...state, cart: newCartArray}
+                const newCartArray = state.cart.map(cartItem => cartItem.id === action.payload.id ? {...cartItem, amount: cartItem.amount + 1} : cartItem);
+                return { ...state, cart: newCartArray};
             } else {
-                return { ...state, cart: [...state.cart, { id: action.payload.id, amount: 1 }]}
+                return { ...state, cart: [...state.cart, { id: action.payload.id, amount: 1 }]};
             }
 
+        case cartActions.DECREMENT_ITEM:
+            const selectedCartItem = state.cart.find(cartItem => cartItem.id === action.payload.id);
+            
+            if (selectedCartItem.amount === 1) {
+                const newCartArray = state.cart.filter(cartItem => cartItem.id !== selectedCartItem.id);
+                return { ...state, cart: newCartArray};
+            } else {
+                const newCartArray = state.cart.map(cartItem => cartItem.id === action.payload.id ? {...cartItem, amount: cartItem.amount - 1} : cartItem);
+                return { ...state, cart: newCartArray};
+            }
+        
+        case cartActions.CLEAR_CART:
+            return { ...state, cart: []}
+
         case cartActions.REMOVE_ITEM:
-            break;
+            const newCartArray = state.cart.filter(cartItem => cartItem.id !== action.payload.id);
+            return { ...state, cart: newCartArray};
 
         default:
             break;
