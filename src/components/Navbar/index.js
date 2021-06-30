57be2341e1cbc607f2cb672d3ba152809f7b8e76
André Figueirôa <alfigueiroab@gmail.com>
2021-06-30 18:52:28 -0300

    Navbar shows the amount of unique items added to cart

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../routes';
 
 export default function Navbar() {
+    const cart = useSelector(state => state.cart.cart);
+
     return (
         <nav>
             <ul>
             <li><Link to={ROUTES.HOME}>HOME</Link></li>
             <li><Link to={ROUTES.ITEMS}>ITEMS</Link></li>
-            <li><Link to={ROUTES.CART}>CART</Link></li>
+            <li><Link to={ROUTES.CART}>CART {cart.length === 0 ? '' : `(${cart.length})`}</Link></li>
             <li><Link to={ROUTES.CREATE_ITEM}>CREATE ITEM</Link></li>
             </ul>
         </nav>
