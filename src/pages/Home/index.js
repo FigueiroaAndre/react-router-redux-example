ad8c1554747acbac765914f31b166609a0da0b9b
André Figueirôa <alfigueiroab@gmail.com>
2021-06-29 21:49:46 -0300

    Add link to others pages from the home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../routes';
 
 export default function Home() {
     return (
-        <p>Home</p>
+        <React.Fragment>
+            <h1>Home</h1>
+            <p>This is a sample application to practice with react-router-dom and react-redux libraries</p>
+            <p>You can check the available items in the <Link to={ROUTES.ITEMS}>Items page</Link></p>
+            <p>You can add a new item to the catalog in the <Link to={ROUTES.CREATE_ITEM}>Create Items page</Link></p>
+            <p>You can check which items you have selected in the <Link to={ROUTES.CART}>Cart page</Link></p>
+        </React.Fragment>
     );
 }
