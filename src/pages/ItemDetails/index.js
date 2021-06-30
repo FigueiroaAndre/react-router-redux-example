1d726a841f4275a8a3cd4e21f2b6063f05509f68
André Figueirôa <alfigueiroab@gmail.com>
2021-06-30 18:50:52 -0300

    Add "add item to cart" functionality to ItemDetails and Items pages

--- src/pages/ItemDetails/index.js
+++ src/pages/ItemDetails/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { pushItem } from '../../store/reducers/cartReducer';
 
 export default function ItemDetails() {
     const { id } = useParams()
     const { name, description, cost } = useSelector(state => state.items.itemList.find(item => item.id === parseInt(id)));
+    const dispatch = useDispatch();
+
 
     return (
         <div>
             <h1>{name}</h1>
             <p>{description}</p>
-            <p>Cost: <span style={{ fontWeight:'bold', fontSize:'2rem', color: 'darkgreen'}}>${cost}</span></p>
-            <button>Add to Cart</button>
+            <p>Cost: <span style={{ fontWeight:'bold', fontSize:'1.3rem', color: 'darkgreen'}}>${cost}</span></p>
+            <button onClick={() => dispatch(pushItem(parseInt(id)))}>Add to Cart</button>
         </div>
     );
 }
--- src/pages/Items/index.js
+++ src/pages/Items/index.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { ROUTES } from '../../routes';
+import { pushItem } from '../../store/reducers/cartReducer';
 
 export default function Items() {
     const items = useSelector(state => state.items);
+    const dispatch = useDispatch();
     const history = useHistory();
 
     function redirectToDetailsPage(id) {
         history.push(`${ROUTES.ITEM_DETAILS}/${id}`);
     }
 
-    return items.itemList.map(item => (
-        <div key={item.id}>
-            <p><span style={{fontWeight: 'bold', display: 'inline'}}>{item.name}</span> cost: ${item.cost}</p>
-            <button onClick={(e) => redirectToDetailsPage(item.id)}>Details</button>
-            <button style={{ marginLeft: 20 }}>Add to Cart</button>
-            <hr/>
-        </div>
-    ));
+    return (
+        <React.Fragment>
+
+        <h1>Items</h1>
+        <hr/>
+            {
+                items.itemList.map(item => (
+                    <div key={item.id}>
+                        <p><span style={{fontWeight: 'bold', display: 'inline'}}>{item.name}</span> cost: ${item.cost}</p>
+                        <button onClick={() => redirectToDetailsPage(item.id)}>Details</button>
+                        <button style={{ marginLeft: 20 }} onClick={() => dispatch(pushItem(item.id))}>Add to Cart</button>
+                        <hr/>
+                    </div>
+                ))
+            }
+        </React.Fragment>
+    );
 }
